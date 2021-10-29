d7b5ad241ae05c39368361ae147257e8a581fe83
mengrru <mengrru@outlook.com>
2021-10-30 01:39:07 +0800

    push all

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # mastodon-on-blog
+
+把你的长毛象消息放在你的博客上！
+
+## 快速开始
+
+将本项目克隆到你的博客目录中，在需要加入该插件的地方插入代码：
+
+```
+<iframe src="<root_path>/mastodon-on-blog/index.html" height=100 width=200></iframe>
+```
+
+保存后刷新页面，就能看到插件显示了。
+
+## 配置
+
+打开 `mastodon-on-blog/index.html`，看到：
+
+```
+MastodonOnBlog({
+    instance: 'o3o.ca',
+    userId: 541,
+    loadingText: '加载中(´·ω·｀)',
+    loadFailText: '加载失败(╯°Д°）╯︵ /(.□ . \\)',
+    token: '',
+    rootDOM: 'my-mastodon-widget',
+    emojiDataPath: './test/emoji.json',
+    testStatusesData: './test/statuses.json'
+})
+```
+
+将 `instance` 改为你所在的长毛象实例，将 `userId` 改为你的数字 id，在 `token` 处填写你的[访问令牌](#设置访问令牌)，最后删除 `emojiDataPath` 和 `testStatusesData` 字段。保存后刷新页面，不出意外就能看到你的长毛象嘟文显示在插件中了。
+
+## 字段说明
+
+## 设置访问令牌
+
+## 主题开发
+
+## 不使用 iframe 插入
---
+++ mastodon-on-blog.js
@@ -0,0 +1,108 @@
+;(function () {
+    function loadFile (path, token, fn, errfn) {
+        const request = new XMLHttpRequest()
+        request.open('get', path)
+        request.setRequestHeader('Authorization', token)
+        request.send(null)
+        request.onload = function () {
+            if (request.readyState === 4) {
+                if (request.status === 200) {
+                    fn(request.responseText)
+                } else {
+                    console.warn('Load file ' + path + ' failed.')
+                    typeof errfn === 'function' ? errfn() : ''
+                }
+            }
+        }
+    }
+    function showImage (e, imageData) {
+        e.path[1].innerHTML = imageData.reduce((a, b) => {
+            if (b.type === 'image') {
+                return a + `<img src="${b.preview_url}" />`
+            }
+            return a
+        }, '')
+    }
+    function render (statusesData, emojisData, config) {
+        let html = ''
+        const getEmoji = getEmojiWrapper(emojisData)
+        for (const d of statusesData) {
+            let content = d.content
+            content = content.replace(/:(\w+):/g, getEmoji)
+            html += `
+                <div class="content">${content}</div>
+            `
+            if (d.media_attachments.some(e => e.type === 'image')) {
+                html += `
+                    <div class="images"><a class="show-image" onclick="MastodonOnBlogProp.showImage(event, ${JSON.stringify(d.media_attachments).replace(/"/g, '\'')})" href="#">显示图片</a></div>
+                `
+            }
+            html += `
+                <div class="time">${formatTime(d.created_at)}</div>
+            `
+        }
+        const rootDOM = document.getElementById(config.rootDOM)
+        const mainDOM = document.createElement('div')
+        mainDOM.id = 'main'
+        mainDOM.innerHTML = html
+        rootDOM.innerHTML = ''
+        rootDOM.appendChild(mainDOM)
+        Array.prototype.slice.call(document.querySelectorAll('.hashtag')).forEach(e => {
+            e.remove()
+        })
+    }
+    function getEmojiWrapper (emojisData) {
+        return function (match, p1) {
+            if (!emojisData) return match
+            const data = emojisData.find(e => e.shortcode === p1)
+            return data ? `<img class="emoji" src="${data.static_url}"/>` : match
+        }
+    }
+    function formatTime (dateString) {
+        const date = new Date(dateString)
+        return `
+        ${date.getFullYear()}-${formatNumber(date.getMonth() + 1)}-${formatNumber(date.getDate())} ${formatNumber(date.getHours())}:${formatNumber(date.getMinutes())}:${formatNumber(date.getSeconds())}`
+    }
+    function formatNumber (num) {
+        return num < 10 ? '0' + num : num
+    }
+    function launch (config) {
+        const rootDOM = document.getElementById(config.rootDOM)
+        rootDOM.innerHTML = config.loadingText
+        loadFile(config.testStatusesData ||
+            `https://${config.instance}/api/v1/accounts/${config.userId}/statuses?tagged=${config.tag || ''}`, config.token,
+            (str) => {
+                const statusesData = JSON.parse(str)
+                loadFile(config.emojiDataPath || `https://${config.instance}/api/v1/custom_emojis`, config.token, (str) => {
+                    const emojisData = JSON.parse(str)
+                    render(statusesData, emojisData, config)
+                }, () => {
+                    render(statusesData, null, config)
+                })
+            }, () => {
+                rootDOM.innerHTML = config.loadFailText
+            })
+    }
+
+    window.MastodonOnBlog = function (config) {
+        if (!config.instance) {
+            throw new Error('required a instance url.')
+        }
+        if (!config.userId) {
+            throw new Error('required a user id.')
+        }
+        if (!config.token) {
+            console.warn('required a token, or use the widget in local only.')
+        }
+        config.loadingText = config.loadingText || '加载中...'
+        config.loadFailText = config.loadFailText || '加载失败'
+        config.rootDOM = config.rootDOM || 'my-mastodon-widget'
+        if (!document.getElementById(config.rootDOM)) {
+            throw new Error('cannot find root DOM #' + config.rootDOM)
+        }
+        launch(config)
+    }
+    window.MastodonOnBlogProp = {
+        showImage: showImage
+    }
+})()
